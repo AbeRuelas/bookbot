10aa1b33da9a1faf31290d29851ea8fd381bab19
AbeRuelas <aberuelas10swe@gmail.com>
2024-06-22 18:21:14 -0700

    Count unique characters in a text file with a dictionary

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
 def main():
     book_path = "books/frankenstein.txt"
-    text = get_book_text(book_path)
-    words = text.split()
-    word_count = len(words)
-    print(word_count)
+    text = get_book_text(book_path).lower()
+    count_chars = count_characters(text)
+    print(count_chars)
 
 def get_book_text(path):
     with open(path) as f:
         return f.read()
+ 
+def count_characters(text_file):
+    charDict = {}
 
-
+    for c in text_file:
+        if c.isalpha():
+            if c in charDict:
+                charDict[c] += 1
+            else:
+                charDict[c] = 1
+    return charDict        
 main()
 

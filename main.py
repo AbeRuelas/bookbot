795bf967516476ce0969a588071a14e22d67e6dc
AbeRuelas <aberuelas10swe@gmail.com>
2024-06-22 17:20:06 -0700

    Add word count to text file

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
-    print(text)
+    words = text.split()
+    word_count = len(words)
+    print(word_count)
 
 def get_book_text(path):
     with open(path) as f:

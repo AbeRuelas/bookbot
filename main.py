69d78e611f0013041771cc2dbaef990d4d0f8b4e
AbeRuelas <aberuelas10swe@gmail.com>
2024-06-23 14:10:24 -0700

    First solution to dict into list of dicts sorted

--- main.py
+++ main.py
@@ -3,7 +3,25 @@ def main():
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     count_chars = count_characters(text)
-    print(count_chars)
+    list_dicts = convert_dict(count_chars)
+    sorted_lists = sort_on(list_dicts)
+    
+    print(f'"-- Begin report of {book_path} --"')
+    print(f'"{num_words} words found in the document"')
+    
+    for item in sorted_lists:
+        print(f"The'{item['char']}' character was found {item['num']} times")
+
+def convert_dict(count_chars):
+    return [{'char':key,'num':value} for key, value in count_chars.items()]
+
+def sort_by_num(d):
+    return d['num']
+
+def sort_on(list_dicts):
+    list_dicts.sort(reverse=True, key=sort_by_num)
+    return list_dicts
+    
 
 def get_num_words(text):
     words = text.split()
@@ -25,4 +43,3 @@ def count_characters(text_file):
                 charDict[low_c] = 1
     return charDict        
 main()
-
